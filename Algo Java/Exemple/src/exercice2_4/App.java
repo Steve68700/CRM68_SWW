b1e0e343fa31d176f9fabb85d0b2a293c9d42b35
Steve68700 <137295483+Steve68700@users.noreply.github.com>
2023-07-07 10:10:53 +0200

    jalon 2

---
+++ Algo Java/Exemple/src/exercice2_4/App.java
@@ -0,0 +1,46 @@
+package exercice2_4;
+import java.util.Scanner;
+/*
+Variable
+annee est un entier
+Debutdu programme
+ecrire "Saisir une Annee : "
+lire <-- annee
+Debut Si
+    Si
+        annee / 4 && /annee /100 && annee /400
+    Alors
+        ecrire "C'est une annee bissextile"
+    Sinon
+        ecrire "Ce n'est pas une annee bissextile"
+    Fin Si
+Fin Si
+*/
+public class App {
+
+	public static void main(String[] args) {
+		//Variable
+		int annee;
+		int mod1;
+		int mod2;
+		int mod3;
+		Scanner sc = new Scanner(System.in);
+		//Debut du programme
+		System.out.println("Saisir une Annee : ");
+		annee = sc.nextInt();
+		mod1 = annee%4;
+		mod2 = annee%100;
+		mod3 = annee%400;
+		if(mod1 == 0 && mod2 != 0 && mod3 == 0)
+		{
+			System.out.println(+ annee+ " est une annee bissextile");
+		}
+		else
+		{
+			System.out.println(+annee+ " n'est pas une annee bissextile");
+		}
+		sc.close();
+
+	}
+
+}
---
+++ Algo Java/Exemple/src/jalon_algo_niveau2_WS/App.java
@@ -0,0 +1,26 @@
+package jalon_algo_niveau2_WS;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		//variables
+		boolean etudiant;
+		String jeudi;
+		String mercredi;
+		int mod1;
+		int mod2;
+		Scanner sc = new Scanner(System.in);
+		//debut du programme
+		System.out.println("Saisir le jour : ");
+		jeudi = sc.nextLine();
+		mercredi = sc.nextLine();
+		System.out.println("Etes vous etudiant ? ");
+		etudiant =sc.equals()
+		
+		
+		sc.close();
+	}
+
+}
