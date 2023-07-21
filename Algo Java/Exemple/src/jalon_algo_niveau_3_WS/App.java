aec297f0787164d8adcbe5892a90f9f895ca9ba2
Steve68700 <137295483+Steve68700@users.noreply.github.com>
2023-07-21 11:50:41 +0200

    maj

--- Algo Java/Exemple/src/jalon_algo_niveau_3_WS/App.java
+++ Algo Java/Exemple/src/jalon_algo_niveau_3_WS/App.java
@@ -8,17 +8,24 @@ public class App {
 		//Variables
 		int nombre;
 		int resultat;
-		int i = 0;
+		int i;
+		String back = "o";
 		Scanner sc = new Scanner(System.in);
 		
 		//Debut du programme
-		System.out.println("Saisir un nombre entre 1 et 10 pour connaitre sa table de multiplication:");
-		nombre = sc.nextInt();
-		for(i=0; i<=10; i++)
+		i = 0;
+		while(back.equals("o"))
 		{
-			System.out.println(i*nombre);
+			System.out.println("Saisir un nombre entre 1 et 10 pour connaitre sa table de multiplication:");
+			nombre = sc.nextInt();
+			for(i=0; i<=10; i++)
+			{
+				resultat = i*nombre;
+				System.out.println(+i+ "x" +nombre+ "=" +resultat);
+			}
+			System.out.println("Souhaitez vous afficher une autre table de multiplication? oui = o");
+			back = sc.next();
 		}
-		
 		
 		
 		sc.close();
