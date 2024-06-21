a828e963fa1dbc100468d0ce1d2a52c9b55ae5b9
Steve68700 <137295483+Steve68700@users.noreply.github.com>
2024-06-21 11:53:58 +0200

    html/css

---
+++ JavaScript/Exo_JS_Couleurs/script.js
@@ -0,0 +1,14 @@
+let bouton1 = document.getElementById("btnRouge");
+let bouton2 = document.getElementById("btnVert");
+let bouton3 = document.getElementById("btnBleu");
+
+let saisie1 = document.getElementById("color1");
+let saisie2 = document.getElementById("color2");
+let saisie3 = document.getElementById("color3");
+
+bouton1.onclick= myFunction()
+
+function myFunction() {
+    
+    document.getElementById(".bodycolor").backgroundColor
+  }
